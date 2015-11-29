d82feada1b28c0f9665dcc16494b43747763baae
alexander <mail@fomalhaut.su>
2015-11-30 00:18:27 +0300

    install.sh fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ -d "dist" ]; then
-    sudo cp dist/passgen /usr/bin;
+    sudo cp dist/pcsv /usr/bin;
     echo "installed successfully";
 else
     echo "the program is not packed"
